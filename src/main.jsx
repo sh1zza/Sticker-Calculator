52361a1aab115147674460a5f108f25e04cae9d7
sh1zza <64583795+sh1zza@users.noreply.github.com>
2026-04-05 20:15:07 +0500

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import StickerCalc from './StickerCalc.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <StickerCalc />
+  </React.StrictMode>,
+)
